a6acb349919c6ac55033d7e14dbb5dd22f37fd8e
torianyap <torianyap@gmail.com>
2022-04-30 13:19:22 +0700

    complete gears form validation

--- assets/scripts/contact-us.js
+++ assets/scripts/contact-us.js
@@ -1,6 +1,6 @@
 const showErr = (msg, label) => {
-  const errDiv = document.createElement("div");
-  errDiv.className = "err-msg";
+  const errDiv = document.createElement('div');
+  errDiv.className = 'err-msg';
   errDiv.innerHTML = msg;
 
   const div = $(`form-${label}`);
@@ -8,46 +8,46 @@ const showErr = (msg, label) => {
 };
 
 const removeErr = () => {
-  const err = document.getElementsByClassName("err-msg");
+  const err = document.getElementsByClassName('err-msg');
   Array.from(err).forEach((item) => item.remove());
 };
 
 const validateForm = () => {
-  const { value: name } = $("name");
-  const { value: email } = $("email");
-  const { value: phone } = $("phone");
-  const { value: service } = $("services");
-  const { value: message } = $("message");
-  const gears = $("gears");
+  const { value: name } = $('name');
+  const { value: email } = $('email');
+  const { value: phone } = $('phone');
+  const { value: service } = $('services');
+  const { value: message } = $('message');
+  const gears = $('gears');
 
   removeErr();
 
   if (!name) {
-    showErr("Name cannot be empty", "name");
+    showErr('Name cannot be empty', 'name');
   } else if (name.length < 4) {
-    showErr("Name must be at least four characters", "name");
+    showErr('Name must be at least four characters', 'name');
   } else if (!email) {
-    showErr("Email cannot be empty", "email");
+    showErr('Email cannot be empty', 'email');
   } else if (!phone) {
-    showErr("Phone cannot be empty", "phone");
+    showErr('Phone cannot be empty', 'phone');
   } else if (!+phone) {
-    showErr("Phone must be numerical format", "phone");
-  } else if (!phone.startsWith("08")) {
-    showErr("Phone must starts with 08", "phone");
+    showErr('Phone must be numerical format', 'phone');
+  } else if (!phone.startsWith('08')) {
+    showErr('Phone must starts with 08', 'phone');
   } else if (phone.length < 10 || phone.length > 14) {
     showErr(
-      "Phone must be at least 10 numbers and maximum 14 characters",
-      "phone"
+      'Phone must be at least 10 numbers and maximum 14 characters',
+      'phone'
     );
-  } else if (gears) {
-
+  } else if (gears && !gears.value) {
+    showErr('Gears cannot be empty', 'gears')
   } else if (!service) {
-    showErr("Service must be filled", "service");
+    showErr('Service must be filled', 'service');
   } else if (!message) {
-    showErr("message must be filled", "message");
+    showErr('message must be filled', 'message');
   } else {
-    alert("Suggestion has been accepted");
-    location.href = "index.html";
+    alert('Suggestion has been accepted');
+    location.href = 'index.html';
   }
 };
 
